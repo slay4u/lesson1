e25ef3f6eb9e1eb1180b9931b3abf1f37a706762
slay4u <galaxy.vd123@gmail.com>
2022-08-24 17:01:45 +0300

    lesson5 commit2

--- src/Dymura_hw5/Figures/Main_Figures.java
+++ src/Dymura_hw5/Figures/Main_Figures.java
@@ -1,36 +1,17 @@
 package Dymura_hw5.Figures;
 
 public class Main_Figures {
-    Figure circle = new Circle(11.3);
-    Figure triangle = new Triangle(11.3);
-    Figure square = new Square(11.3);
-    Figure [] figures = new Figure[3];
-
-    public void squares(){
+    public static void main(String[] args) {
+        Basic [] basics = new Basic[5];
+        basics[0] = new Circle(13.9);
+        basics[1] = new Square(11.3);
+        basics[2] = new Triangle(11.3, 9.5, 14.2);
+        basics[3] = new Square(23.6);
+        basics[4] = new Triangle(10.1, 6.4, 4.6);
         double allS = 0;
-        for (int i = 0; i < figures.length; i++) {
-            if (figures.equals(circle)) {
-                allS += ((Circle) circle).square();
-            } else if (figures.equals(triangle)) {
-                allS += ((Triangle) triangle).square();
-            } else {
-                allS += ((Square) square).square();
-            }
+        for (int i = 0; i < basics.length; i++) {
+            allS += basics[i].square();
         }
         System.out.println("All squares are: " + allS);
-    }
-
-    public static void main(String[] args) {
-        Circle circle = new Circle(11.3);
-        circle.square();
-        Triangle triangle = new Triangle(11.3);
-        triangle.square();
-        Square square = new Square(11.3);
-        square.square();
-        Figure [] figures = new Figure[3];
-        figures[0] = circle;
-        figures[1] = triangle;
-        figures[2] = square;
-
     }
 }
